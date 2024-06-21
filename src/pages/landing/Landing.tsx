be15d0347beb5d62c1eca3da6a99508896d9b230
Kevin <kevin@eaucole.com>
2024-06-22 02:14:01 +0800

    Attempt to make landing page responsive

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 const Landing = () => {
     return (
         <div className="hero min-h-screen bg-gray-50 overflow-x-hidden mt-[-10px]">
-            <div className="hero-content text-center">
-                <div className="max-w-lg">
-                    <div className="badge py-4 px-5 border-gray-200">
-                        <span className="me-1 font-bold">100% free</span> and 
-                        <a className="ms-1 hover:text-blue-600 underline" href="https://github.com/kevinhellos/notii-planner" target="_blank">open source</a>
+            <div className="hero-content text-center flex flex-col items-center">
+                <div className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+                    <div className="badge py-4 px-5 border-gray-200 flex justify-center items-center mx-auto">
+                        <span className="me-1 font-bold">100% free</span> 
+                        <a className="ms-1 hover:text-blue-600 underline" href="https://github.com/kevinhellos/notii-planner" target="_blank" rel="noopener noreferrer">open source</a>
                     </div>
-                    <h1 className="join text-5xl font-extrabold mt-5">
-                        <img src="/assets/imgs/note.png" className="-rotate-12 mr-5" width={50} alt="" />
+                    <h1 className="join text-3xl lg:text-5xl font-extrabold mt-5 flex items-center justify-center">
+                        <img src="/assets/imgs/note.png" className="-rotate-12 mr-2 md:mr-5" width={40} alt="" />
                         Notii Planner
                     </h1>
-                    <p className="py-6">
+                    <p className="py-6 text-base md:text-lg lg:text-xl">
                         All-in-one planner app designed to help you stay organized, productive, and stress-free
                     </p>
                     <Link to="/login" className="btn bg-[#E64B3C] hover:bg-[#E64B3C] text-white font-medium rounded-full px-8">
@@ -24,7 +24,7 @@ const Landing = () => {
                             <div className="input border border-base-300">https://notii.site</div>
                         </div>
                         <div className="flex justify-center border-t border-base-300">
-                            <img src="/assets/imgs/demo1.png" alt="Notii planner dashboard" />
+                            <img src="/assets/imgs/demo1.png" alt="Notii planner dashboard" className="w-full" />
                         </div>
                     </div>
                 </div>
@@ -32,5 +32,5 @@ const Landing = () => {
         </div>
     );
 }
- 
+
 export default Landing;
